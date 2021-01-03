f0e7beb40730e60b6f03433d1e5e27401f7a6516
wjcutrone <73838888+wjcutrone@users.noreply.github.com>
2021-01-03 11:28:36 -0500

    Create main.py

---
+++ Pybank/main.py
@@ -0,0 +1,41 @@
+#import modules
+import os
+import csv
+#set path to csv
+budget_csv=os.path.join("Resources/budget_data.csv")
+#set up csv reader
+with open(budget_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #Make lists to later use to manipulate data
+    months=[]
+    profit_loss=[]
+    monthly_change=[]
+
+    next(csvreader)
+    #set up data analysis of the csv  
+    for row in csvreader:
+        months.append(row[0])
+        profit_loss.append(int(row[1]))
+    #Find total months
+    print("Total Months: ", str(len(months)))
+    #Find total profit/loss
+    print("Total:$ "+str(sum(profit_loss)))
+    #Find average change of profit/loss
+    for i in range(len(profit_loss)-1):
+        monthly_change.append((profit_loss[i+1]-profit_loss[i]))
+    average_monthly_change=sum(monthly_change)/len(monthly_change)
+    average_monthly_change=str(round(average_monthly_change,2))
+    print("Average Change: "+"$"+str(average_monthly_change))
+    #Greatest Increase in Profit/Loss and correspoing month
+    max_monthly_change=max(monthly_change)
+    index_monthly_change = 0
+    for i in range(len(monthly_change)):
+        if monthly_change[i]==max_monthly_change:
+            index_monthly_change = i
+            break
+
+    print("Greatest Increase in Profits: "+months[index_monthly_change+1]+" "+str(max_monthly_change))
+
+
+
+   
